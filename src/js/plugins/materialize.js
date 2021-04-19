6e376c44173c1e2702129881c484e117ec88f1c7
Anton <pro.grishin@gmail.com>
2021-04-19 22:12:37 +0300

    ui and styles

--- src/js/plugins/materialize.js
+++ src/js/plugins/materialize.js
@@ -4,6 +4,28 @@ import 'materialize-css/dist/js/materialize.min.js';
 const select = document.querySelectorAll('select');
 M.FormSelect.init(select);
 
-export function getSelectInstance(elem){
+export function getSelectInstance(elem) {
     return M.FormSelect.getInstance(elem);
+}
+
+const autocomplete = document.querySelectorAll('.autocomplete');
+M.Autocomplete.init(autocomplete, {
+    data: {
+        "Apple": null,
+        "Microsoft": null,
+        "Google": 'https://placehold.it/250x250'
+    }
+});
+
+export function getAutocompleteInstance(elem) {
+    return M.Autocomplete.getInstance(elem);
+}
+
+const datepicker = document.querySelectorAll('.datepicker');
+M.Datepicker.init(datepicker, {
+    showClearBtn: true,
+});
+
+export function getDatePickerInstance(elem) {
+    return M.Datepicker.getInstance(elem);
 }
