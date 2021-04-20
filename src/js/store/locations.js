4d28db9df4fa7b44810c87023d9e6c216459adc5
Anton <pro.grishin@gmail.com>
2021-04-20 19:05:11 +0300

    get logo

--- src/js/store/locations.js
+++ src/js/store/locations.js
@@ -5,23 +5,36 @@ class Locations {
     this.api = api;
     this.countries = null;
     this.cities = null;
-    this.shortCitiesList = null;
+    // this.shortCitiesList = null;
+    // this.shortCities = {};
+    // this.lastSearch = {};
+    this.airlines = {};
   }
   async init() {
     const response = await Promise.all([
       this.api.countries(),
       this.api.cities(),
+      this.api.airlines()
     ]);
 
-    const [countries, cities] = response;
+    const [countries, cities, airlines] = response;
     this.countries = this.serializeCountries(countries);
     this.cities = this.serializeCities(cities);
     this.shortCitiesList = this.createShortCitiesList(this.cities);
+    this.airlines = this.serializeAirLines(airlines);
+    console.log(this.airlines);
     return response;
   }
 
   getCityCodeByKey(key) {
     return this.cities[key].code;
+  }
+
+  getAirlineNameByCode(code){
+    return this.airlines[code]?this.airlines[code].name : '';
+  }
+  getAirlineLogoByCode(code){
+    return this.airlines[code]?this.airlines[code].logo : '';
   }
 
   createShortCitiesList(cities) {
@@ -30,6 +43,16 @@ class Locations {
       acc[key] = null;
       return acc;
     }, {});
+  }
+
+  serializeAirLines(airlines) {
+    return airlines.reduce((acc, item) => { 
+      console.log(item.code);
+      item.logo = `http://pics.avs.io/200/200/${item.code}.png`;
+      item.name = item.name || item.name_translations.en;
+      acc[item.code] = item;
+      return acc;
+    }, {})
   }
 
   serializeCountries(countries) {
@@ -46,7 +69,10 @@ class Locations {
       const country_name = this.getCountryNameByCode(city.country_code);
       const city_name = city.name || city.name_translations.en;
       const key = `${city.name},${country_name}`;
-      acc[key] = city;
+      acc[key] = {
+        ...city,
+        country_name,
+      };
       return acc;
     }, {})
   }
