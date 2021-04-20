a02ce088486c53e014288995bc90e6be99746751
Anton <pro.grishin@gmail.com>
2021-04-20 15:45:19 +0300

    get input field value and autocomplete

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,40 @@
 import locations from './store/locations'
 import '../css/style.css';
 import './plugins';
+import formUI from './views/form';
 
-locations.init().then(res => {
-  console.log(res);
-  console.log(locations);
-  console.log(locations.getCitiesByCountryCode('AG'));
+document.addEventListener('DOMContentLoaded', () => {
+  initApp();
+  const form = formUI.form;
+
+
+  //Events
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    onFormSubmit();
+  });
+
+  // Handlers
+
+  async function initApp() {
+    await locations.init();
+    formUI.setAutocompleteData(locations.shortCitiesList);
+  }
+
+  async function onFormSubmit() {
+    // Собрать данные из input
+    const origin = formUI.originValue;
+    const destination = formUI.destinationValue;
+    const depart_date = formUI.departDateValue;
+    const return_date = formUI.returnDateValue;
+
+    console.log(origin, destination, depart_date, return_date);
+  }
 });
+
+
+// locations.init().then(res => {
+//   console.log(res);
+//   console.log(locations);
+//   console.log(locations.getCitiesByCountryCode('AG'));
+// });
--- src/js/store/locations.js
+++ src/js/store/locations.js
@@ -5,6 +5,7 @@ class Locations {
     this.api = api;
     this.countries = null;
     this.cities = null;
+    this.shortCitiesList = null;
   }
   async init() {
     const response = await Promise.all([
@@ -13,14 +14,57 @@ class Locations {
     ]);
 
     const [countries, cities] = response;
-    this.countries = countries;
-    this.cities = cities;
+    this.countries = this.serializeCountries(countries);
+    this.cities = this.serializeCities(cities);
+    this.shortCitiesList = this.createShortCitiesList(this.cities);
     return response;
   }
-  getCitiesByCountryCode(code){
+
+  getCityCodeByKey(key) {
+    return this.cities[key].code;
+  }
+
+  createShortCitiesList(cities) {
+    return Object.entries(cities).reduce((acc, [key]) => {
+      // console.log(key);
+      acc[key] = null;
+      return acc;
+    }, {});
+  }
+
+  serializeCountries(countries) {
+    // { 'Country code':{ ... } }
+    return countries.reduce((acc, country) => {
+      acc[country.code] = country;
+      return acc;
+    }, {})
+  }
+
+  serializeCities(cities) {
+    // { 'City name, Country name': { ... } }
+    return cities.reduce((acc, city) => {
+      const country_name = this.getCountryNameByCode(city.country_code);
+      const city_name = city.name || city.name_translations.en;
+      const key = `${city.name},${country_name}`;
+      acc[key] = city;
+      return acc;
+    }, {})
+  }
+
+  getCountryNameByCode(code) {
+    // { 'Country code':{ ... } }
+    return this.countries[code].name;
+  }
+
+  getCitiesByCountryCode(code) {
     return this.cities.filter(city => city.country_code === code);
   }
 }
 
 const locations = new Locations(api);
 export default locations;
+
+// { 'City, Country': null }
+// [{},{}]
+// { 'City' : { ... } }=>cities[code]
+// 
